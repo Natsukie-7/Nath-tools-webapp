ff0e778d14e1797f342463912d39a004a4a378cb
Nath <nathanfxs17@gmail.com>
2025-10-13 11:05:36 -0300

    Updates

---
+++ assets/Home--lLSHzDX.js
@@ -0,0 +1,1 @@
+import{s as o,u as t,c as s}from"./index-CGAYqfbR.js";const r=o("div")``,c=n=>{const[,{toggleTheme:e}]=t();return s(r,{onClick:()=>{e()},children:"Home"})};export{c as default};
--- assets/Home-DtBt3MZ8.js
+++
@@ -1,1 +0,0 @@
-import{s as o,u as t,c as s}from"./index-BYWqUaJ3.js";const r=o("div")``,c=n=>{const[,{toggleTheme:e}]=t();return s(r,{onClick:()=>{e()},children:"Home"})};export{c as default};
